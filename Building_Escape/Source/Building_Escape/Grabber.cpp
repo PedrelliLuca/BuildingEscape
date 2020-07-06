baed473dc9a4b70d2c97962e4569c152f1f5fb18
Luca Pedrelli <pedrelli.luca@gmail.com>
2020-07-06 22:57:10 +0200

    :collision: Ray tracing added to grabber component.

--- Building_Escape/Source/Building_Escape/Grabber.cpp
+++ Building_Escape/Source/Building_Escape/Grabber.cpp
@@ -1,7 +1,12 @@
 // Copyright Luca Pedrelli 2020.
 
-
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h" // GetWorld()
+#include "GameFramework/PlayerController.h" // For viewport
 #include "Grabber.h"
+
+// Macro to make clear where we define out parameters
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -29,6 +34,51 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player's viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+	
+	// UE_LOG(LogTemp, Warning, TEXT("ViewPointLocation: %s ViewPointRotation: %s"),
+	// 	*PlayerViewPointLocation.ToString(),
+ 	// 	*PlayerViewPointRotation.ToString()
+	// );
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	// To visually test our LineTraceEnd
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(0, 255, 0),
+		false,
+		0.f,
+		0,
+		5.f
+	);
+
+	// Ray casting
+	FHitResult Hit;
+	// We're not using tags in order to query whether we've collided with something (trace tag), we
+	// can therefore leave FName empty.
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		// Only meshes with PhysicsBody channel are detected
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), 
+		TraceParams
+	);
+	
+	// Logging the actor we hit
+	AActor* ActorHit = Hit.GetActor();
+	if(ActorHit) // Jesus please protect me from nullptrs
+		UE_LOG(LogTemp, Warning, TEXT("Hit actor %s!"), *ActorHit->GetName());
 }
 
